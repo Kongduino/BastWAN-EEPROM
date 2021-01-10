ab85e6fdd7e4d10bc3c7ec4897f4fe8a88fcac38
Kongduino <dbvic@me.com>
2021-01-10 18:37:31 +0800

    First commit

---
+++ BastWAN_EEPROM.ino
@@ -0,0 +1,59 @@
+#include <Wire.h>
+
+#include "SparkFun_External_EEPROM.h"
+// Click here to get the library: http://librarymanager/All#SparkFun_External_EEPROM
+ExternalEEPROM myMem;
+unsigned char buf[256];
+
+void setup() {
+  SerialUSB.begin(115200);
+  SerialUSB.flush();
+  delay(3000);
+  Serial.println("\nQwiic EEPROM example");
+  Serial.println("Buffer size: " + String(SERIAL_BUFFER_SIZE));
+  Wire.begin(SDA, SCL);
+  Wire.setClock(100000);
+  if (myMem.begin() == false) {
+    Serial.println("No memory detected. Freezing.");
+    while (1)
+      ;
+  }
+  uint32_t myLen = myMem.length(), index = 0;
+  Serial.println("Memory detected!");
+  Serial.print("Mem size in bytes: ");
+  Serial.println(myLen);
+  myLen = 2048;
+  // Just a few blocks
+  while (myLen > 0) {
+    for (uint16_t i = 0; i < 256; i++) buf[i] = myMem.read(index++);
+    hexDump(256);
+    myLen -= 256;
+  }
+}
+
+void loop() {
+}
+
+void hexDump(uint16_t len) {
+  String s = "|", t = "| |";
+  Serial.println(F("  |.0 .1 .2 .3 .4 .5 .6 .7 .8 .9 .a .b .c .d .e .f |"));
+  Serial.println(F("  +------------------------------------------------+ +----------------+"));
+  for (uint16_t i = 0; i < len; i += 16) {
+    for (uint8_t j = 0; j < 16; j++) {
+      if (i + j >= len) {
+        s = s + "   "; t = t + " ";
+      } else {
+        char c = buf[i + j];
+        if (c < 16) s = s + "0";
+        s = s + String(c, HEX) + " ";
+        if (c < 32 || c > 127) t = t + ".";
+        else t = t + (String(c));
+      }
+    }
+    uint8_t index = i / 16;
+    Serial.print(index, HEX); Serial.write('.');
+    Serial.println(s + t + "|");
+    s = "|"; t = "| |";
+  }
+  Serial.println(F("  +------------------------------------------------+ +----------------+"));
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# BastWAN_EEPROM
+
+A project demonstrating how to use an EEPROM on the BastWAN.
+
+
+![HexDump](HexDump.png)
